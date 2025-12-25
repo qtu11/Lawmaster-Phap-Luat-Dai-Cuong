aada9b43e46b979a2f2243da0aeed96cf9c92943
qtu11 <trachduong93@gmail.com>
2025-12-25 22:24:39 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,6 +3,7 @@ import questions300 from './questions_300.json';
 
 export const APP_NAME = "LawMaster";
 export const QUIZ_DURATION_SECONDS = 45 * 60; // 45 minutes
+export const QUESTIONS_FILE_NAME = './questions_300.json';
 
 export const QUESTIONS: Question[] = (questions300 as any).map((q: any) => ({
   id: q.id,
